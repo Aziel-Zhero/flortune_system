3efe85e7d13afb172e725443890739ead6ce4046
Aziel Zhero <aziel.outerheaven25@gmail.com>
2025-10-23 10:55:32 +0000

    Try fixing this error: `Runtime Error: Error: [next-auth]: `useSession`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from 'next';
 import { AppSettingsProvider } from '@/contexts/app-settings-context';
 import { Toaster } from "@/components/ui/toaster";
+import { SessionProvider } from '@/components/auth/session-provider'; // Importado
 import './globals.css'; 
 import { Suspense } from 'react';
 
@@ -28,10 +29,12 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         <Suspense fallback={<div>Carregando...</div>}>
-          <AppSettingsProvider>
-            {children}
-            <Toaster />
-          </AppSettingsProvider>
+          <SessionProvider>
+            <AppSettingsProvider>
+              {children}
+              <Toaster />
+            </AppSettingsProvider>
+          </SessionProvider>
         </Suspense>
       </body>
     </html>
