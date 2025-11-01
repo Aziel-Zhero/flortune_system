0ff09e22462f9e0721675b6922beeb3fb998cb9b
Aziel Zhero <aziel.outerheaven25@gmail.com>
2025-11-01 19:00:46 +0000

    ficou bom vejo que algumas telas como na transações o limite não ta send

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -13,12 +13,14 @@ export default function AppLayout({
   
   return (
     <SidebarProvider defaultOpen>
-      <div className="flex min-h-screen flex-col bg-background">
+      <div className="flex min-h-screen w-full flex-col bg-background">
         <AppHeader />
-        <div className="flex flex-1 pt-16"> 
+        <div className="flex h-full">
           <AppSidebar />
-          <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
-            {children}
+          <main className="flex-1 overflow-y-auto pl-[var(--sidebar-width)] pt-16 group-data-[state=collapsed]:pl-[var(--sidebar-width-icon)] md:transition-[padding-left] md:ease-linear">
+            <div className="p-4 md:p-6 lg:p-8">
+              {children}
+            </div>
           </main>
         </div>
       </div>
