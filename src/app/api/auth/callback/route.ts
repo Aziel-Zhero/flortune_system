2b77f50ea2f632cb20968cebce79476669382897
Aziel Zhero <aziel.outerheaven25@gmail.com>
2026-03-06 23:22:59 +0000

    apresentou esse erro no deploy pode corrigir ? 8:19:25 PM: $ npm run bui

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,5 +1,4 @@
 
-// src/app/api/auth/callback/route.ts
 import { createClient } from '@/lib/supabase/server';
 import { NextResponse } from 'next/server';
 import { type NextRequest } from 'next/server';
